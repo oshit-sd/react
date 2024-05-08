e8f90de954acabfbf2c94d3f7fc8fc1a334485ee
= <=>
2024-05-08 11:20:51 +0600

    design some modify

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,22 @@ function App() {
           />
 
           {/* Task List */}
-          <div className="space-y-2 overflow-auto max-h-60 pb-5 border-b border-gray-300">
-            <div className="flex items-center">
+          <ul className="space-y-2 overflow-auto max-h-60 pb-5 border-b border-gray-300">
+            <li className="flex items-center">
               <input type="checkbox" className="form-checkbox mr-2" />
               <p className="flex-1 line-through">Task 1</p>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-500 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M16.95 4.95a1 1 0 010 1.41L11.41 10l5.54 5.54a1 1 0 01-1.41 1.41L10 11.41l-5.54 5.54a1 1 0 01-1.41-1.41L8.59 10 3.05 4.46a1 1 0 011.41-1.41L10 8.59l5.54-5.54a1 1 0 011.41 0z" clipRule="evenodd" />
               </svg>
-
-            </div>
-            <div className="flex items-center">
+            </li>
+            <li>
               <input type="checkbox" className="form-checkbox mr-2" />
-              <p className="flex-1">Task 2</p>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-500 cursor-pointer" viewBox="0 0 20 20" fill="currentColor">
+              Task 1
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-500 cursor-pointer float-end" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M16.95 4.95a1 1 0 010 1.41L11.41 10l5.54 5.54a1 1 0 01-1.41 1.41L10 11.41l-5.54 5.54a1 1 0 01-1.41-1.41L8.59 10 3.05 4.46a1 1 0 011.41-1.41L10 8.59l5.54-5.54a1 1 0 011.41 0z" clipRule="evenodd" />
               </svg>
-
-            </div>
-          </div>
+            </li>
+          </ul>
 
           {/* Check All Button */}
           <div className="flex justify-between items-center my-5 border-b pb-5 text-gray-600">
